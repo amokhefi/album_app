04bcd88958da99063aab1896732785a37fbc4805
amokhefi <Nescafeebole1->
2022-03-27 10:34:43 +0200

    define data transfer object received by the api

---
+++ app/src/main/java/com/abd/dev/album/data/remote/dto/AlbumDto.kt
@@ -0,0 +1,9 @@
+package com.abd.dev.album.data.remote.dto
+
+data class AlbumDto(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
