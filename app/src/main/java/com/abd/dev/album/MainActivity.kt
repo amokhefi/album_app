48cbcc73a04f653a8b5ae0fa122c83f997a3ba35
amokhefi <Nescafeebole1->
2022-03-27 10:23:30 +0200

    basic project structure

---
+++ app/src/main/java/com/abd/dev/album/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.abd.dev.album
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abd.dev.album.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
