a02b997de4745ed935356398aa4967370d5b7cf7
amokhefi <Nescafeebole1->
2022-03-27 11:46:40 +0200

    define album entity that represents the album table in db

---
+++ app/src/main/java/com/abd/dev/album/data/local/model/AlbumEntity.kt
@@ -0,0 +1,14 @@
+package com.abd.dev.album.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class AlbumEntity(
+    @PrimaryKey
+    val id: Int,
+    val albumId: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
