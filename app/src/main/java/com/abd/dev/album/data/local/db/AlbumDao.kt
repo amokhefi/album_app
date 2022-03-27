7440154dab884125c4c2934ac71b174121951c17
amokhefi <Nescafeebole1->
2022-03-27 11:53:47 +0200

    basic AlbumDao to perform db operations

---
+++ app/src/main/java/com/abd/dev/album/data/local/db/AlbumDao.kt
@@ -0,0 +1,21 @@
+package com.abd.dev.album.data.local.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.abd.dev.album.data.local.model.AlbumEntity
+
+@Dao
+interface AlbumDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(album: AlbumEntity)
+
+    @Query("SELECT * from AlbumEntity WHERE id =:id")
+    suspend fun findAlbumById(id: Int): AlbumEntity?
+
+    @Query("SELECT * from AlbumEntity WHERE albumId =:albumId ORDER BY title")
+    suspend fun findAlbumByCategory(albumId: Int): List<AlbumEntity>
+
+}
